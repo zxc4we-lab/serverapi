20ff7bc93394540259d7a02786ea2c659729fe54
zxc4we-lab <zxc4we028@gmail.com>
2024-08-31 17:05:20 +0700

    Update v6.sh

--- v6.sh
+++ v6.sh
@@ -13,9 +13,9 @@ gen64() {
 }
 install_3proxy() {
     echo "installing 3proxy"
-    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+    URL="https://github.com/z3APA3A/3proxy-archive/raw/master/0.8.9/3proxy-0.8.9.tgz"
     wget -qO- $URL | bsdtar -xvf-
-    cd 3proxy-3proxy-0.8.6
+    cd 3proxy
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
     cp src/3proxy /usr/local/etc/3proxy/bin/
